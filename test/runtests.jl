26781bad5cc3194fa87b87d807d47bf13027ab55
Marek Kaluba <kalmar@amu.edu.pl>
2021-06-21 18:16:04 +0200

    remove the old implementation

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,42 +2,32 @@ using Test
 import AbstractAlgebra
 using Groups
 
-include("symmetric.jl")
-using LinearAlgebra
+using KnuthBendix
+
+using GroupsCore
+include(joinpath(pathof(GroupsCore), "..", "..", "test", "conformance_test.jl"))
 
 @testset "Groups" begin
 
-   @testset "wlmetric_ball" begin
-      M = AbstractAlgebra.MatrixAlgebra(AbstractAlgebra.zz, 3)
-      w = one(M); w[1,2] = 1;
-      r = one(M); r[2,3] = -3;
-      s = one(M); s[1,3] = 2; s[3,2] = -1;
+    @testset "wlmetric_ball" begin
+        M = AbstractAlgebra.MatrixAlgebra(AbstractAlgebra.zz, 3)
+        w = one(M); w[1,2] = 1;
+        r = one(M); r[2,3] = -3;
+        s = one(M); s[1,3] = 2; s[3,2] = -1;
 
-      S = [w,r,s]; S = unique([S; inv.(S)]);
-      _, sizes = Groups.wlmetric_ball(S, radius=4);
-      @test sizes == [7, 33, 141, 561]
-      _, sizes = Groups.wlmetric_ball_serial(S, radius=4);
-      @test sizes == [7, 33, 141, 561]
-   end
+        S = [w,r,s]; S = unique([S; inv.(S)]);
+        _, sizes = Groups.wlmetric_ball(S, radius=4);
+        @test sizes == [7, 33, 141, 561]
+        _, sizes = Groups.wlmetric_ball_serial(S, radius=4);
+        @test sizes == [7, 33, 141, 561]
+    end
 
-   include("FreeGroup-tests.jl")
-   include("AutGroup-tests.jl")
-   include("FPGroup-tests.jl")
+    include("free_groups.jl")
+    include("fp_groups.jl")
 
-   @testset "New FPGroups" begin
-      using Groups.New
-      using KnuthBendix
+    include("AutFn.jl")
 
-      using GroupsCore
-      include(joinpath(pathof(GroupsCore), "..", "..", "test", "conformance_test.jl"))
-
-      include("free_groups.jl")
-      include("fp_groups.jl")
-
-      include("AutFn.jl")
-
-      if !haskey(ENV, "CI")
-         include("benchmarks.jl")
-      end
-   end
+    # if !haskey(ENV, "CI")
+    #    include("benchmarks.jl")
+    # end
 end
