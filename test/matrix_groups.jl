22a8809c6fad1a3c4da789e71a65a7ced1f1f918
Marek Kaluba <kalmar@amu.edu.pl>
2022-04-02 14:53:06 +0200

    rm AbstractAlgebra

--- test/matrix_groups.jl
+++ test/matrix_groups.jl
@@ -1,7 +1,6 @@
 using Groups.MatrixGroups
 
 @testset "Matrix Groups" begin
-
     @testset "SL(n, ℤ)" begin
         SL3Z = SpecialLinearGroup{3}(Int8)
 
@@ -9,7 +8,20 @@ using Groups.MatrixGroups
 
         E, sizes = Groups.wlmetric_ball(S, radius=4)
 
-        @test sizes = [13, 121, 883, 5455]
+        @test sizes == [13, 121, 883, 5455]
+
+        E(i,j) = SL3Z([A[MatrixGroups.ElementaryMatrix{3}(i,j, Int8(1))]])
+
+        A = alphabet(SL3Z)
+        w = E(1,2)
+        r = E(2,3)^-3
+        s = E(1,3)^2*E(3,2)^-1
+
+        S = [w,r,s]; S = unique([S; inv.(S)]);
+        _, sizes = Groups.wlmetric_ball(S, radius=4);
+        @test sizes == [7, 33, 141, 561]
+        _, sizes = Groups.wlmetric_ball_serial(S, radius=4);
+        @test sizes == [7, 33, 141, 561]
 
         @testset "GroupsCore conformance" begin
             test_Group_interface(SL3Z)
@@ -18,7 +30,5 @@ using Groups.MatrixGroups
 
             test_GroupElement_interface(g, h)
         end
-
     end
-
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using Test
-import AbstractAlgebra
 using Groups
 
 import KnuthBendix: Word
@@ -8,19 +7,6 @@ using GroupsCore
 include(joinpath(pathof(GroupsCore), "..", "..", "test", "conformance_test.jl"))
 
 @testset "Groups" begin
-
-    @testset "wlmetric_ball" begin
-        M = AbstractAlgebra.MatrixAlgebra(AbstractAlgebra.zz, 3)
-        w = one(M); w[1,2] = 1;
-        r = one(M); r[2,3] = -3;
-        s = one(M); s[1,3] = 2; s[3,2] = -1;
-
-        S = [w,r,s]; S = unique([S; inv.(S)]);
-        _, sizes = Groups.wlmetric_ball(S, radius=4);
-        @test sizes == [7, 33, 141, 561]
-        _, sizes = Groups.wlmetric_ball_serial(S, radius=4);
-        @test sizes == [7, 33, 141, 561]
-    end
 
     include("free_groups.jl")
     include("fp_groups.jl")
